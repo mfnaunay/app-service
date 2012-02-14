9cb0e1b2e446ac696e0784bb4c2e77743780f6a0
Miguel Naunay <miguel.fabricio@aunasoft.com>
2012-02-14 15:33:06 -0500

    prueba de intellisense

---
+++ Demo.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: miguel.fabricio
+ * Date: 14/02/12
+ * Time: 15:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+$saludo = 'prueba de intellisence...';
+if($saludo != null)
+        echo $saludo;
+
